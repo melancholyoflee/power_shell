f5f0330b6c1e0beecf2ed481986f78e8e2fd9610
melancholyoflee <saiyji6saiyiji6@yahoo.com.tw>
2022-11-17 15:57:47 +0800

    add changeName.ps1

---
+++ changeName.ps1
@@ -0,0 +1,7 @@
+
+
+
+
+rename-Item 'C:\Users\Lee\Desktop\1.txt' -NewName 'temp.txt'  
+rename-Item 'C:\Users\Lee\Desktop\2.txt' -NewName '1.txt'  
+rename-Item 'C:\Users\Lee\Desktop\temp.txt' -NewName '2.txt'  
